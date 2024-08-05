81497d4439205fd4a21a6d3d2a53a814f0309642
Medom05 <145607268+Medom5@users.noreply.github.com>
2024-08-05 20:14:22 +0300

    solved : exercice 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+  const args = [...arguments];
+  return arr.filter((e) => args.indexOf(e) === -1);
 };
 
 // Do not edit below this line
