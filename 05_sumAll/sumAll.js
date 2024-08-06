cca626af1491c40338f3ac8cbb552d0a60b00066
Medom05 <145607268+Medom5@users.noreply.github.com>
2024-08-06 10:51:15 +0300

    solved : exercice 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (isNaN(a) || typeof(b) !== "number" || a < 0 || b < 0 || a % 1 !== 0 || b % 1 !== 0)
+    return "ERROR";
 
+    if (a == b) return a;
+    
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  return a + sumAll(a + 1, b);
 };
 
 // Do not edit below this line
